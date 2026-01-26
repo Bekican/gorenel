22b3187dfb655dcd4582ab27762dd75055bc0a94
Bekir Can <bekircan3634@gmail.com>
2026-01-27 01:34:44 +0300

    logger.go

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,1 +1,23 @@
 package logger
+
+import (
+	"sync"
+
+	"go.uber.org/zap"
+)
+
+// global logger'ı tanımlıyoruz
+var (
+	Log  *zap.Logger
+	once sync.Once
+)
+
+// config logger configurations
+type Config struct {
+	Level       string
+	OutputPath  string
+	Development bool
+	Encoding    string
+}
+
+//sensible defaults
