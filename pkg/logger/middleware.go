55d405f3ece782a808c5eb9089b6aa3000cea4d8
Bekir Can <bekircan3634@gmail.com>
2026-01-30 02:34:02 +0300

    middleware-devam

--- pkg/logger/middleware.go
+++ pkg/logger/middleware.go
@@ -1,1 +1,11 @@
 package logger
+
+import (
+	"net/http"
+)
+
+// response writer http.ResponseWriterı kapsıyor -> statusCode'u öğrenmek için
+type ResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
