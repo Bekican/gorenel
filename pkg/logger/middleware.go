2c032de6af21937e4f74a0e43a031dba0305e38e
Bekir Can <bekircan3634@gmail.com>
2026-02-02 14:45:00 +0300

    middleware.go-devam

--- pkg/logger/middleware.go
+++ pkg/logger/middleware.go
@@ -2,10 +2,38 @@ package logger
 
 import (
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 )
 
 // response writer http.ResponseWriterı kapsıyor -> statusCode'u öğrenmek için
-type ResponseWriter struct {
+type responseWriter struct {
 	http.ResponseWriter
 	statusCode int
+}
+
+func newResponseWriter(w http.ResponseWriter) *responseWriter {
+	return &responseWriter{ResponseWriter: w, statusCode: http.StatusOK}
+}
+
+func (rw *responseWriter) WriteHeader(code int) {
+	rw.statusCode = code
+	rw.ResponseWriter.WriteHeader(code)
+}
+
+// http loglarını yazar -> zamanlama ve içerikleriyle
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		requestID := r.Header.Get("X-Request")
+		if requestID == "" {
+			requestID = uuid.New().String()
+		}
+
+		//response headerına request id ekledik
+		w.Header().Set("X-Request-ID", requestID)
+
+	})
 }
