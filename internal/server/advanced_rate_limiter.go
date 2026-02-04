fef3b62f0abf8b6e6360a6dfcc381cbd35d5c034
Bekir Can <bekircan3634@gmail.com>
2026-02-04 18:34:48 +0300

    advanced_rate_limiter_devam

--- internal/server/advanced_rate_limiter.go
+++ internal/server/advanced_rate_limiter.go
@@ -1,1 +1,22 @@
 package server
+
+import (
+	"sync"
+	"time"
+)
+
+type Quota struct {
+	Limit      int
+	WindowSize time.Duration
+}
+
+type UsageLog struct {
+	TimeStamps []time.Time
+}
+
+type AdvancedRateLimiter struct {
+	mu       sync.RWMutex
+	usage    map[string]*UsageLog
+	tiers    map[string]Quota
+	userTier map[string]string
+}
