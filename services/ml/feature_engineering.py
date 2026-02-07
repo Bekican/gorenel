fee8e72cb3c6456555f71d7917240a7082506dee
Bekir Can <bekircan3634@gmail.com>
2026-02-07 20:51:41 +0300

    model-egitimi-devam1

---
+++ services/ml/feature_engineering.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import RobustScaler
+import hashlib
+import logging
+
+logger = logging.getLogger(__name__)
+
+# class FeatureEngineer:
+#     def __init__(self):
+        
