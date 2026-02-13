b3ab805f597f17c70da9e04d9fb72f1d420a6aac
Bekir Can <bekircan3634@gmail.com>
2026-02-13 20:54:18 +0300

    proje-proda-uygun-hale-getiriliyor2

--- internal/server/ai_gateway.go
+++ internal/server/ai_gateway.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"net/http"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -26,4 +27,13 @@ type AIGateway struct {
 	Providers map[string]AIProvider
 	Logger    *zap.Logger
 	AICache   *RedisCache
+}
+
+func (g *AIGateway) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	pName := r.URL.Query().Get("provider")
+	provider, ok := g.Providers[pName]
+
+	if !ok {
+
+	}
 }
